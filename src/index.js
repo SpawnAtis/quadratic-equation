6e3d315ad92a6967bfd5ba15fb701a83b1e17a6c
Vlad <guf_of_vlad@mail.ru>
2018-02-21 23:57:02 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+    let splitEquation = equation.split(' '),
+        a = +splitEquation[0],
+        b = +(splitEquation[3] + splitEquation[4]),
+        c = +(splitEquation[7] + splitEquation[8]);
+
+    let D = (b * b) - 4 * a * c;
+    if (D < 0) { console.log('The roots are complex numbers'); return }
+
+    let x1, x2;
+    if (!D) {
+        x1 = x2 = - (b / (2*a));
+    } else {
+        if (b > 0 && c > 0)
+        x1 = (-b + Math.sqrt(D)) / (2 * a);
+        x2 = (-b - Math.sqrt(D)) / (2 * a);
+    }
+
+    return [Math.round(x1), Math.round(x2)].sort((a, b) => a - b );
+};
